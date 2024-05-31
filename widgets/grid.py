b98d46c089bf5225d1e1f860403b43129ae6dd50
Maya Chang <mcc9@hawaii.edu>
2024-05-30 14:57:36 -1000

    update
    updating with loelle's code

--- widgets/grid.py
+++ widgets/grid.py
@@ -1,4 +1,26 @@
 import tkinter as tk
+from datetime import datetime, timedelta
+
+from read_data import read_data
+
+# Global variables
+# Custom row and column dimensions
+cell_size = 20 # default
+header_width = 110
+map_height = 100
+date_height = 90
+time_height = 80
+
+# Calculate starting y-coordinates
+date_y = map_height
+time_y = map_height + date_height
+events_y = map_height + date_height + time_height
+
+# Initial grid size
+num_rows = 7
+num_cols = 40
+max_x = header_width + num_cols * cell_size
+max_y = events_y + num_rows * cell_size
 
 class Grid:
     def __init__(self, master):
@@ -11,60 +33,72 @@ class Grid:
         self.canvas.configure(xscrollcommand=self.scrollbar_x.set)
 
         self.draw_grid()
+        self.populate_data()
 
     def draw_grid(self):
-        # Custom row and column dimensions
-        map_height = 100
-        date_colheight = map_height + 80
-        time_colheight = date_colheight + 80
-        label_rowwidth = 80
+        # Main gridlines
+        self.canvas.create_line(0, date_y, max_x, date_y, fill="gray") # date
+        self.canvas.create_line(0, time_y, max_x, time_y, fill="gray") # time
+        self.canvas.create_line(0, events_y, max_x, events_y, fill="gray") # events
+        self.canvas.create_line(header_width, 0, header_width, max_y, fill="gray") # header column
 
-        self.canvas.create_line(0, 0, 1000, 0, fill="gray", tags="gridline") # top row
-        self.canvas.create_line(0, map_height, 1000, map_height, fill="gray", tags="map") # map
-        self.canvas.create_line(0, date_colheight, 1000, date_colheight, fill="gray", tags="utc") # date
-        self.canvas.create_line(0, time_colheight, 1000, time_colheight, fill="gray", tags="utc") # time
-        self.canvas.create_line(label_rowwidth, 0, label_rowwidth, time_colheight+200, fill="gray", tags="label") # header column
-
-        num_rows = -1
         # Create rows
-        for i in range(time_colheight, time_colheight+220, 20):
-            self.canvas.create_line(0, i, 1000, i, fill="gray", tags="gridline")
-            num_rows += 1
-        
-        num_cols = 0
+        for i in range(0, num_rows + 1): # +1 to draw the last line
+            y = events_y + i * cell_size
+            self.canvas.create_line(0, y, max_x, y, fill="gray")
+   
         # Create columns
-        for i in range(label_rowwidth, 1000, 20): 
-            self.canvas.create_line(i, time_colheight, i, time_colheight+200, fill="gray", tags="gridline")
-            num_cols += 1
+        for i in range(0, num_cols + 1): # +1 to draw the last line
+            x = header_width + i * cell_size
+            self.canvas.create_line(x, events_y, x, max_y, fill="gray")
 
-        # fill in labels
-        self.canvas.create_text(40, map_height/2, text="Map", fill="white")
-        self.canvas.create_text(40, (date_colheight/2) + (map_height/2), text="UTC Date", fill="white")
-        self.canvas.create_text(40, (time_colheight/2) +(date_colheight/2), text="UTC Time", fill="white")
+        # Fill in headers
+        self.canvas.create_text(header_width/2, map_height/2, text="Map", fill="white")
+        self.canvas.create_text(header_width/2, date_y + date_height/2, text="UTC Date", fill="white")
+        self.canvas.create_text(header_width/2, time_y + time_height/2, text="UTC Time", fill="white")
         
-        self.canvas.create_text(40, 10 + time_colheight, text="Umbra", fill="white")
-        self.canvas.create_text(40, 30 + time_colheight, text="Orbital Events", fill="white")
+        self.canvas.create_text(header_width/2, events_y + cell_size/2, text="Umbra", fill="white")
+        self.canvas.create_text(header_width/2, events_y + cell_size*1.5, text="Orbital Events", fill="white")
 
-        # fill in the ground station labels
-        start = 30
-        row_label = 1
-        for row in range(time_colheight, time_colheight+160, 20):
-            start = start + 20
-            self.canvas.create_text(40, start + time_colheight, text="GS"+str(row_label), fill="white")
-            row_label += 1
-
-        colors = ["gray", "white", "red", "orange", "yellow", "green", "blue", "purple", "red", "orange"]
+        # Fill in the ground station headers
+        gs_names = ["MC3-NPS", "MC3-PCH", "KSAT-PA", "KSAT-PL", "KSAT-NZ"]
+        gs_start = events_y + cell_size * 2.5 # .5 to vertically center text
+        for i in range(len(gs_names)):
+            self.canvas.create_text(header_width/2, gs_start + cell_size*i, text=gs_names[i], fill="white")
 
         # Fill in data
-        for row in range(num_rows):  # iterate through rows
-            cell_color = colors[row]
-            for col in range(num_cols):  # iterate through columns
-                #cell_color = "white"
-                x0 = label_rowwidth + col * 20  # Starting x-coordinate for the cell
-                y0 = time_colheight + row * 20  # Starting y-coordinate for the cell
-                x1 = x0 + 20  # Ending x-coordinate for the cell
-                y1 = y0 + 20  # Ending y-coordinate for the cell
-                self.canvas.create_rectangle(x0, y0, x1, y1, fill=cell_color)
-                
-        # Adjust scroll region based on the actual size of the grid
+        # colors = ["gray", "white", "red", "orange", "yellow", "green", "blue", "purple", "red", "orange"]
+        # for row in range(num_rows):  # iterate through rows
+        #     cell_color = colors[row]
+        #     for col in range(num_cols):  # iterate through columns
+        #         #cell_color = "white"
+        #         x0 = header_width + col * 20  # Starting x-coordinate for the cell
+        #         y0 = events_y + row * 20  # Starting y-coordinate for the cell
+        #         x1 = x0 + 20  # Ending x-coordinate for the cell
+        #         y1 = y0 + 20  # Ending y-coordinate for the cell
+        #         self.canvas.create_rectangle(x0, y0, x1, y1, fill=cell_color)
+        
+        # Adjust scroll region based on the actual size of the grid, remove for infinite scroll
         self.canvas.config(scrollregion=self.canvas.bbox("all"))
+
+    def populate_data(self):
+        data = read_data('ground_contacts.csv')
+
+        # Plot starting date and time
+        start_datetime = datetime.fromisoformat(data[0]["utc"][:-1])
+        rounded_time = start_datetime - (start_datetime - datetime.min) % timedelta(minutes=30) # round down to nearest 30 min
+        self.canvas.create_text(header_width, date_y + date_height/2, text=rounded_time.strftime('%Y-%m-%d'), fill="white", angle=90)
+        # self.canvas.create_text(header_width, time_y + time_height/2, text=rounded_time.strftime('%H:%M:%S'), fill="white", angle=90)
+
+        # Label every 30 minute increment
+        increment = 30
+        for i in range(0, max_x//cell_size, increment//5):
+            self.canvas.create_text(header_width + i*cell_size, time_y + time_height/2, text=rounded_time.strftime('%H:%M:%S'), fill="white", angle=90) 
+            rounded_time = rounded_time + timedelta(minutes=increment)
+        
+        # for contact in data:
+        #     utc = contact['utc']
+        #     hst = contact['hst']
+        #     gs_id = contact['GS id']
+        #     orbital_events = contact['orbital events']
+        #     self.canvas.create_text(100, 100, text=f"{utc}, {hst}, {gs_id}, {orbital_events}", fill="white")

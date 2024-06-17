72b83357e06d9d7b24d41c3264dac6186069b4ea
Maya Chang <mcc9@hawaii.edu>
2024-06-17 11:27:57 -1000

    updates

--- read_json.py
+++ read_json.py
@@ -2,35 +2,37 @@ from datetime import datetime
 from datetime import timezone
 import json
 
-def read_json(list):
+def read_json():
     # # open JSON file
     file = open('data.json')
 
-    # # return JSON object as a dictionary
-    # data = json.load(file)
+    list = []
 
-    # for i in data:
+    # return JSON object as a dictionary
+    data = json.load(file)
+
+    for i in data:
     #     utc = i['utc']
     #     date = datetime.fromisoformat(utc[:-1]).astimezone(timezone.utc)
     #     # print(date)
 
-    #     gs_id = i['gs-id']
+        event_name = i['event_name']
     #     # print(gs_id)
 
     #     orb_event = i['orbital-event']
     #     # print(orb_event)
 
-    #     if gs_id not in list:
-    #         list.append(gs_id)
+        if event_name not in list:
+            list.append(event_name)
 
-    # # print list of gs ids
+    # print list of gs ids
     # print(list)
     
-    # # close json file
-    # file.close()
+    # close json file
+    file.close()
 
-    # # return list of gs ids
-    # return list
+    # return list of gs ids
+    return list
 
     # # read in gs-id
     # # check if gs-id has been saved before or not
